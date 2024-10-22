690fd1c4f0ec689bf1141ef2ae25cd085e7b634e
ShutovKS <schutow.kir@yandex.ru>
2024-10-22 11:49:59 +0300

    init unity project

---
+++ src/Futurift Plugin/Assets/Plugins/Futurift/DataSenders/UdpPortSender.cs
@@ -0,0 +1,31 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using Futurift.Options;
+
+namespace Futurift.DataSenders
+{
+    internal class UdpPortSender : IDataSender
+    {
+        private readonly UdpClient udpClient;
+        private readonly IPEndPoint endPoint;
+
+        public UdpPortSender(UdpOptions options)
+        {
+            udpClient = new UdpClient();
+            endPoint = new IPEndPoint(IPAddress.Parse(options.ip), options.port);
+        }
+
+        public void SendData(byte[] data)
+        {
+            udpClient.Send(data, data.Length, endPoint);
+        }
+
+        public void Start()
+        {
+        }
+
+        public void Stop()
+        {
+        }
+    }
+}
