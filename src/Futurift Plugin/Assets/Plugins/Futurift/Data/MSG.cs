007c1514e65489a77f39f85f3a4f76ff0d023824
ShutovKS <schutow.kir@yandex.ru>
2025-01-11 18:16:21 +0300

    Remove old files

--- src/Futurift Plugin/Assets/Plugins/Futurift/Data/MSG.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Futurift.Data
-{
-    internal static class MSG
-    {
-        public const byte SOM = byte.MaxValue;
-        public const byte EOM = 254;
-        public const byte ESC = 253;
-    }
-}
